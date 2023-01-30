50f526298807e392554f6f2a0f2a159f2adcdbde
Rowend Duke <rowendduke36@gmail.com>
2023-01-30 05:36:48 +0000

    chore: switched to repl express server instead of static hosting

--- replit.nix
+++ replit.nix
@@ -1,7 +1,5 @@
 { pkgs }: {
   deps = [
-    pkgs.nodejs-16_x
-    pkgs.nodejs-16_x
     pkgs.edit
     pkgs.xsel
     pkgs.nodejs-16_x
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express()
+const port = 80
+
+
+app.use(express.static("build"));
+app.use(function(req, res, next) {
+  console.log(req.url);
+  if (req.accepts('html')) {
+    res.send(fs.readFile);
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.json({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+app.use(function(req, res, next, err) {
+  console.error(err);
+})
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}`)
+})
